110e161eb6add0c32f4686a581bd2536cc7061fe
Chris <cristi552kr@ymail.com>
2019-01-09 13:27:41 +0000

    Create invalidmailmsg.js

---
+++ invalidmailmsg.js
@@ -0,0 +1,32 @@
+﻿function InvalidNameMsg(textbox) {
+
+    if (textbox.validity.patternMismatch) {
+        textbox.setCustomValidity('Please enter your name');
+    }
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
+
+function InvalidPostCodeMsg(textbox) {
+
+    if (textbox.validity.patternMismatch) {
+        textbox.setCustomValidity('Please enter your UK post code');
+    }
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
+
+function InvalidPhoneMsg(textbox) {
+
+    if (textbox.validity.patternMismatch) {
+        textbox.setCustomValidity('Please enter an UK phone number starting with 0');
+    }
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
